84001be1e28a8c810c45df08e0aac4f69ff77a08
kiitoss <antoinechataignier1998@orange.fr>
2022-06-24 13:32:24 +0200

    BREAKING CHANGE : set KACF as a class

--- KACF/functions.php
+++ KACF/functions.php
@@ -25,3 +25,24 @@ function kacf_admin_css()
     wp_enqueue_style('kacf-admin-css', get_template_directory_uri() . '/lib/less/index.php?fichier=admin');
 }
 add_action('admin_print_styles', 'kacf_admin_css', 11);
+
+
+
+function kacf_enable_debug()
+{
+    add_action('template_redirect', function () {
+        // Turn on error reporting.
+        error_reporting(E_ALL);
+
+        // Sets to display errors on screen. Use 0 to turn off.
+        ini_set('display_errors', 1);
+
+        // Sets to log errors. Use 0 (or omit) to not log errors.
+        ini_set('log_errors', 1);
+
+        // Sets a log file path you can access in the theme editor.
+        $log_path = get_template_directory() . '/debug.txt';
+        ini_set('error_log', $log_path);
+    });
+}
+kacf_enable_debug();
---
+++ KACF/lib/kacf/kacf.inc.php
@@ -0,0 +1,139 @@
+<?php
+
+class kacf
+{
+    static public $VERSION = "v0.1.0";
+    private $allowed_blocks = array();
+    private $tpl = '';
+    private $tpluri = '';
+
+    public function __construct($gutpath = null)
+    {
+        // exit if the class is not load in a WP environement
+        if (!class_exists('WP')) {
+            trigger_error("You must use WordPress to use the KACF class.", E_USER_ERROR);
+            die();
+        }
+        // exit if the class is not load in an ACF environement
+        if (!class_exists('ACF')) {
+            trigger_error("You must use ACF to use the KACF class.", E_USER_ERROR);
+            die();
+        }
+
+        // define gutenberg repesitory paths
+        $this->tpl = get_template_directory() . $gutpath;
+        $this->tpluri = get_template_directory_uri() . $gutpath;
+    }
+
+    public function get_registered_blocks()
+    {
+        return $this->allowed_blocks;
+    }
+
+    /**
+     * Get the ACF block data from the PHP file
+     * The data can be found in the header of the template file
+     */
+    private function get_acf_block_comment_data($file_path)
+    {
+        // use the wordpress built-in function get_file_data
+        $data = get_file_data(
+            $file_path,
+            array(
+                'title' => 'title',
+                'icon' => 'icon',
+                'keywords' => 'keywords',
+                'post_types' => 'post_types',
+                'description' => 'description',
+                'category' => 'category',
+            )
+        );
+
+        // set default icon
+        if (!$data['icon']) $data['icon'] = 'align-full-width';
+
+        // split keywords
+        $keywords = $data['keywords']
+            ? preg_split('/[, ]/', $data['keywords'])
+            : array();
+        $data['keywords'] = $keywords;
+
+        // split post types or set default
+        $post_types = $data['post_types']
+            ? preg_split('/[, ]/', $data['post_types'])
+            : array('page');
+        $data['post_types'] = $post_types;
+
+        // set the render template of the block
+        $data['render_template'] = $file_path;
+
+        return $data;
+    }
+
+    /**
+     * Register a new ACF block
+     */
+    private function register_acf_block($data, $relative_path)
+    {
+        // register the block with its data
+        acf_register_block_type(array(
+            'name'              => $data['name'],
+            'title'             => __($data['title'], 'kacf'),
+            'description'       => $data['description'],
+            'category'          => $data['category'],
+            'render_template'   => $data['render_template'],
+            'icon'              => $data['icon'],
+            'keywords'          => $data['keywords'],
+            'post_types'        => $data['post_types'],
+            'mode'              => 'edit',
+            'enqueue_style' => $this->tpluri . $relative_path . '/less/index.php',
+            'enqueue_script' => $this->tpluri . $relative_path . '/js/script.js',
+        ));
+
+        // load the ACF field generator if exists
+        $acf_php_file_path = $this->tpl . $relative_path . '/acf/acf.php';
+        if (file_exists($acf_php_file_path) && filesize($acf_php_file_path)) {
+            include $acf_php_file_path;
+        }
+
+        // allow user to use this new block
+        $this->allowed_blocks[] = 'acf/' . $data['name'];
+    }
+
+    /**
+     * Register new ACF blocks directly took from the folder
+     */
+    public function register_acf_blocks()
+    {
+        // check the possibility to add new blocks
+        if (!function_exists('acf_register_block_type')) return;
+
+        // get all directories under the gutenberg directory
+        $dirs = array_filter(glob($this->tpl . '*'), 'is_dir');
+
+        // loop through these directories
+        foreach ($dirs as $dir) {
+            // get the file path
+            $file_path = $dir . '/template.php';
+
+            // retrieve the data in the header comments of the file
+            $data = $this->get_acf_block_comment_data($file_path);
+
+            // get the directory relative path
+            // necessary to enqueue styles and scripts
+            $relative_path = implode('', explode($this->tpl, $dir));
+
+            // generate the name of the block with the folder name
+            $data['name'] = preg_replace('/[^a-z0-9\-\_]/', '', str_replace(' ', '-', strtolower($relative_path)));
+
+            // set the block title if not exists
+            if (!$data['title']) $data['title'] = __($data['name'], 'kacf');
+
+            // set the folder name as the first keyword
+            array_unshift($data['keywords'], $data['name']);
+
+            // register the new acf block
+            $this->register_acf_block($data, $relative_path);
+        }
+    }
+}
